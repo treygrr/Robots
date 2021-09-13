de36b591a499fbe9e6299c7f24610f8b690669e9
Trey <phlasc@gmail.com>
2021-09-13 00:01:11 -0400

    Add base templating and grab data needed for simulations

--- src/components/leaderboard/LeaderboardStats.vue
+++ src/components/leaderboard/LeaderboardStats.vue
@@ -1,14 +1,34 @@
 <template>
-    <article class="leaderboard-controls">
-        <div class="playerWrapper">
-            <div class="player">
-            </div>
-        </div>
-    </article>
+  <article class="leaderboard-controls">
+    <div class="playerWrapper">
+      <div v-for="robot in robots" :key="robot.id" class="player">
+        {{ robot }}
+      </div>
+    </div>
+  </article>
 </template>
 
 <script>
-export default {
+  export default {
     name: 'LeaderboardStats',
-}
+    data: function () {
+      return {
+        robots: [],
+      }
+    },
+    mounted: function () {
+      this.setRobots();
+    },
+    methods: {
+      setRobots: function () {
+        this.$store.state.robots.robots.forEach(robot => {
+          let robotData = {
+            robot,
+            tasks: this.$store.state.robots.robotsTasks.filter(function (task) { return task.robotId === robot.id}),
+          }
+          this.robots.push(robotData);
+        });
+      }
+    },
+  }
 </script>
