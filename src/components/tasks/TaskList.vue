35b6ec3f9e91ead83753cf5ef34e7a78059215ef
Trey <phlasc@gmail.com>
2021-09-13 00:00:46 -0400

    Fix store typo

--- src/components/tasks/TaskList.vue
+++ src/components/tasks/TaskList.vue
@@ -26,7 +26,7 @@
         return this.$store.state.robotTypes.robotTypes.find(robot => robot.id === robotId).name;
       },
       deleteTask: function(taskId) {
-        this.$store.state.commit('tasks/DELETE_TASK', taskId);
+        this.$store.commit('tasks/DELETE_TASK', taskId);
       },
     },
   }
