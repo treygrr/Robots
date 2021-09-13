f07600a842f00d6cc1ff81457b69ef66c33214b6
Trey <phlasc@gmail.com>
2021-09-13 14:06:26 -0400

    Update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Thank you RV</h1>
+    <p style="text-align: left">Thank you so much for taking the time to look at my code. I hope you enjoy it!</p>
+    <p style="text-align: left">
+      I'm a super anxious tech nerd who loves to code. I enjoy learning new things and solving problems. 
+      I am always looking for new ways to improve my skills and knowledge.
+      As a result, I have a lot of fun picking up new languages and frameworks. 
+      Just about anything is possible! You just have to be curious.
+    </p>
+    <p style="text-align: left">Thank you, <br><br>Trey</p>
+
   </div>
 </template>
+<style scoped>
+  .about {
+    max-width: 700px;
+    margin: 0 auto;
+  }
+  p {
+    padding: 5px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+</style>
